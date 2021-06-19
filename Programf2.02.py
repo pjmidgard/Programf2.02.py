85c1254d65763a8829545eaf8e0211d472d624c8
Java Software <pj.midgard@yahoo.com>
2021-06-19 20:40:59 +0100

    Add files via upload

--- Programf2.02.py
+++ Programf2.02.py
@@ -255,7 +255,7 @@ class compression:
                                             # Define a writer to compress a flat raw binary file.
                                             w = Writer(chunk_duration=1.)
                                             # Open the file to compress.
-                                            w.open('data.bin', sample_rate=30000., n_channels=256, dtype=np.int64)
+                                            w.open('data.bin', sample_rate=30000., n_channels=+1, dtype=np.int16)
                                             # Compress it into a compressed binary file, and a JSON header file.
                                             w.write('data.cbin', 'data.ch')
                                             w.close()
